aa3f816ce8f00dcba3cb6f7bf1a2b6af7ee90d66
neilstats <neilstats@users.noreply.github.com>
2022-04-06 17:19:56 +0100

    change plotting of text on forest plots
    Plot estimate and CI text without leaving additional space for minus signs. (This means the parentheses around CIs may not be aligned.)

--- R/ONYXForestValues.r
+++ R/ONYXForestValues.r
@@ -39,7 +39,8 @@ ONYXForestValues <- function(estim, upper, lower, stderr=NULL, isLogged=TRUE, xa
 		}
 	
 		xcoord <- xaxmax+spacing
-		if (estim[i] < 0) xcoord <- xcoord - strwidth("-", font=Boldness[i], cex=Font)
+		# NW: Don't move position of text if estimate is negative
+		# if (estim[i] < 0) xcoord <- xcoord - strwidth("-", font=Boldness[i], cex=Font)
 	
 		if (ShowCI[i] == 0) {
 			text(x=xcoord+strwidth("0.00", cex=Font, font=Boldness[i]), y=YLocs[i], ValueLabels[[1]][i], font=Boldness[i], adj=c(1, 0.5), cex=Font)
@@ -53,55 +54,60 @@ ONYXForestValues <- function(estim, upper, lower, stderr=NULL, isLogged=TRUE, xa
 			text(x=xcoord, y=YLocs[i], labels=printval, adj=c(0,0.5), font=Boldness[i], cex=Font)
 			next
 		}
-		if (CIpositive) {
 		
-			ci_part <- paste("(", ValueLabels[[2]][i], separator, ValueLabels[[3]][i], ")", sep="")
-			text(x=xcoord+strwidth("0.00", cex=Font, font=Boldness[i]), y=YLocs[i], ValueLabels[[1]][i], font=Boldness[i], adj=c(1, 0.5), cex=Font)
-			text(x=xcoord+strwidth("0.00 ", cex=Font, font=Boldness[i]), y=YLocs[i], ci_part, font=Boldness[i], adj=c(0, 0.5), cex=Font)
-			
-		} else {
+		# NW: Just plot Estimate and CI at this position (i.e. do not leave additional space for minus signs)
+		ci_part <- paste(ValueLabels[[1]][i], " (", ValueLabels[[2]][i], separator, ValueLabels[[3]][i], ")", sep="")
+		text(x=xcoord, y=YLocs[i], ci_part, font=Boldness[i], adj=c(0, 0.5), cex=Font)
 		
-			# now we need to position the labels manually, which is going to be a massive faff
-			zeroprint <- ifelse(is.null(digits), sprintf("%.2f", 0), sprintf(paste("%.", digits, "f", sep=""), 0))
-			xadjust <- ifelse(substr(ValueLabels[[1]][i], 1, 1)=="-", strwidth(paste("-", zeroprint, " (", sep=""), font=Boldness[i], cex=Font), strwidth(paste(zeroprint, " (", sep=""), font=Boldness[i], cex=Font))
-			
-			text(x=xcoord + xadjust, y=YLocs[i], paste(ValueLabels[[1]][i], " (", sep=""), font=Boldness[i], adj=c(1, 0.5), cex=Font)
-			
-			# position the labels such that we allow for a minus sign
-			lci <- paste(ValueLabels[[2]][i], separator, sep="")
-			if (substr(lci,1,1) == "-") {
-				if (length(grep(".",lci, fixed=TRUE))==1) {
-					xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (", sep=""), font=Boldness[i], cex=Font)
-				} else {
-					xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (.", sep=""), font=Boldness[i], cex=Font)
-				}
-			} else {
-				if (length(grep(".",lci, fixed=TRUE))==1) {
-					xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (-", sep=""), font=Boldness[i], cex=Font)
-				} else {
-					xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (-.", sep=""), font=Boldness[i], cex=Font)
-				}
-			}
-			text(x=xcoord, y=YLocs[i], lci, font=Boldness[i], adj=c(0, 0.5), cex=Font)
-			
-			# now the UCI
-			uci <- paste(ValueLabels[[3]][i], ")", sep="")
-			if (substr(uci,1,1) == "-") {
-				if (length(grep(".", uci, fixed=TRUE)) == 1) {
-					xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",", sep=""), font=Boldness[i], cex=Font)
-				} else {
-					xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",.", sep=""), font=Boldness[i], cex=Font)
-				}
-			} else {
-				if (length(grep(".", uci, fixed=TRUE)) == 1) {
-					xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",-", sep=""), font=Boldness[i], cex=Font)
-				} else {
-					xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",-.", sep=""), font=Boldness[i], cex=Font)
-				}
-			}
-			text(x=xcoord, y=YLocs[i], uci, font=Boldness[i], adj=c(0, 0.5), cex=Font)
-			
-		}
+		# if (CIpositive) {
+		# 
+		# 	ci_part <- paste("(", ValueLabels[[2]][i], separator, ValueLabels[[3]][i], ")", sep="")
+		# 	text(x=xcoord+strwidth("0.00", cex=Font, font=Boldness[i]), y=YLocs[i], ValueLabels[[1]][i], font=Boldness[i], adj=c(1, 0.5), cex=Font)
+		# 	text(x=xcoord+strwidth("0.00 ", cex=Font, font=Boldness[i]), y=YLocs[i], ci_part, font=Boldness[i], adj=c(0, 0.5), cex=Font)
+		# 	
+		# } else {
+		# 
+		# 	# now we need to position the labels manually, which is going to be a massive faff
+		# 	zeroprint <- ifelse(is.null(digits), sprintf("%.2f", 0), sprintf(paste("%.", digits, "f", sep=""), 0))
+		# 	xadjust <- ifelse(substr(ValueLabels[[1]][i], 1, 1)=="-", strwidth(paste("-", zeroprint, " (", sep=""), font=Boldness[i], cex=Font), strwidth(paste(zeroprint, " (", sep=""), font=Boldness[i], cex=Font))
+		# 	
+		# 	text(x=xcoord + xadjust, y=YLocs[i], paste(ValueLabels[[1]][i], " (", sep=""), font=Boldness[i], adj=c(1, 0.5), cex=Font)
+		# 	
+		# 	# position the labels such that we allow for a minus sign
+		# 	lci <- paste(ValueLabels[[2]][i], separator, sep="")
+		# 	if (substr(lci,1,1) == "-") {
+		# 		if (length(grep(".",lci, fixed=TRUE))==1) {
+		# 			xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (", sep=""), font=Boldness[i], cex=Font)
+		# 		} else {
+		# 			xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (.", sep=""), font=Boldness[i], cex=Font)
+		# 		}
+		# 	} else {
+		# 		if (length(grep(".",lci, fixed=TRUE))==1) {
+		# 			xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (-", sep=""), font=Boldness[i], cex=Font)
+		# 		} else {
+		# 			xcoord <- xaxmax +spacing + strwidth(paste(zeroprint, " (-.", sep=""), font=Boldness[i], cex=Font)
+		# 		}
+		# 	}
+		# 	text(x=xcoord, y=YLocs[i], lci, font=Boldness[i], adj=c(0, 0.5), cex=Font)
+		# 	
+		# 	# now the UCI
+		# 	uci <- paste(ValueLabels[[3]][i], ")", sep="")
+		# 	if (substr(uci,1,1) == "-") {
+		# 		if (length(grep(".", uci, fixed=TRUE)) == 1) {
+		# 			xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",", sep=""), font=Boldness[i], cex=Font)
+		# 		} else {
+		# 			xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",.", sep=""), font=Boldness[i], cex=Font)
+		# 		}
+		# 	} else {
+		# 		if (length(grep(".", uci, fixed=TRUE)) == 1) {
+		# 			xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",-", sep=""), font=Boldness[i], cex=Font)
+		# 		} else {
+		# 			xcoord <- xaxmax+spacing + strwidth(paste(zeroprint, " (-", zeroprint, ",-.", sep=""), font=Boldness[i], cex=Font)
+		# 		}
+		# 	}
+		# 	text(x=xcoord, y=YLocs[i], uci, font=Boldness[i], adj=c(0, 0.5), cex=Font)
+		# 	
+		# }
 
 
 	}
